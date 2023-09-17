5a1ec5eee84aa734c3e32e64d3b3aaaf89c32cd4
Van Hoang <nbdev82@gmail.com>
2023-09-18 00:44:46 +0700

    initial project

---
+++ src/main/java/com/myproject/game/store/app/v1/resources/dao/HomeDAO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.myproject.game.store.app.v1.resources.dao;
+
+/**
+ *
+ * @author Van Hoang
+ */
+public interface HomeDAO {
+    
+}
---
+++ src/main/java/com/myproject/game/store/app/v1/resources/dao/impl/HomeDAOImpl.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.myproject.game.store.app.v1.resources.dao.impl;
+
+import com.myproject.game.store.app.v1.resources.dao.HomeDAO;
+
+/**
+ *
+ * @author Van Hoang
+ */
+public class HomeDAOImpl implements HomeDAO{
+    
+}
