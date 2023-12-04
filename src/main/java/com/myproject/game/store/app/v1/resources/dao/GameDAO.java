7a35884d301198100235217d84f564bb98364983
cong09112003 <congnguyenhuu2703@gmail.com>
2023-12-04 20:09:33 +0700

    DAO search

--- src/main/java/com/myproject/game/store/app/v1/resources/dao/GameDAO.java
+++ src/main/java/com/myproject/game/store/app/v1/resources/dao/GameDAO.java
@@ -5,6 +5,7 @@
 package com.myproject.game.store.app.v1.resources.dao;
 
 import com.myproject.game.store.app.v1.resources.model.entity.Game;
+import java.sql.Timestamp;
 import java.util.List;
 
 /**
@@ -13,4 +14,10 @@ import java.util.List;
  */
 public interface GameDAO {
     public Game getGameById(Long gameId);
+    public List<Game> getListGamesByString(String txtSearch);
+    public void RemoveGameByGameId(Long gameId);
+    public void InsertGame(Long main_img_id,String game_name,String title,
+            String content,String warning,String award,Timestamp release_date, int initial_price,
+            boolean is_discount,int discount_price,String dev_name,String pub_name,int percent_pos,
+            String global_review,int numb_review);
 }
